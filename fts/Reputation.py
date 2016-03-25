071a1ad825ca9c04e24b1adc7047590eb8dcd9d2
9dev <9devmail@gmail.com>
2016-03-25 16:17:07 +0100

    Created empty functional tests suite.

---
+++ fts/Reputation.py
@@ -0,0 +1,25 @@
+from ._base import BaseTestCase
+
+
+class TestReputation(BaseTestCase):
+
+    def test_can_see_his_reputation_score(self):
+        self.fail()
+
+    def test_can_see_his_level(self):
+        self.fail()
+
+    def test_can_see_user_reputation_score_in_admin_panel(self):
+        self.fail()
+
+    def test_can_see_user_level_in_admin_panel(self):
+        self.fail()
+
+    def test_can_gain_reputation_for_performing_actions(self):
+        self.fail()
+
+    def test_can_level_up(self):
+        self.fail()
+
+    def test_can_see_his_reputation_history(self):
+        self.fail()
